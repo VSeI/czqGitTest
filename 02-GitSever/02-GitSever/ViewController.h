43815e45934288ac06f0a5f2c8555332fe341d3c
陈志强 <623782285@qq.com>
2015-10-08 11:56:21 +0800

    GitSever提交

---
+++ 02-GitSever/02-GitSever/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  02-GitSever
+//
+//  Created by 陈志强 on 15/10/8.
+//  Copyright (c) 2015年 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 02-GitSever/02-GitSever/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  02-GitSever
+//
+//  Created by 陈志强 on 15/10/8.
+//  Copyright (c) 2015年 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
